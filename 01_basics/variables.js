a8c6a23f975e8c986b6a720fb18fdafee033e34c
Buddha shyam govardhana naidu <shyamgovardhan09@gmail.com>
2025-07-15 14:18:02 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=123
+let accountemail="hc@gmail.com"
+var accountpass=121
+let accountcity="vizag"
+let accountstate;
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/ 
+console.table([accountemail,accountpass,accountcity,accountstate])
