4008be9b4772c41abd41c80576efda6a6fb88e0e
Buddha shyam govardhana naidu <shyamgovardhan09@gmail.com>
2025-07-15 14:18:13 +0000

    variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+//number --> 2 to power 53
+//bigint
+//string -->""
+//boolean -->true/false
+//null -->standalone value
+//undefined --> no assignment of value
+//symbol --> to find uniqueness.
+
+//object
